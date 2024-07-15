e16ac654a2141cf46cfe6038e826948ac36acb22
mattia33011 <mattia.iaria30@gmail.com>
2024-07-16 00:30:22 +0200

    init commit

---
+++ src/main/kotlin/it/mag/wrongtzap/WrongtzapApplication.kt
@@ -0,0 +1,11 @@
+package it.mag.wrongtzap
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WrongtzapApplication
+
+fun main(args: Array<String>) {
+	runApplication<WrongtzapApplication>(*args)
+}
---
+++ src/test/kotlin/it/mag/wrongtzap/WrongtzapApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.mag.wrongtzap
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WrongtzapApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
