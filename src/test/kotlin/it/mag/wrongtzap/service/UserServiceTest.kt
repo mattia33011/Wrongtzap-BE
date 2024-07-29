37bcbe4d71add833a64e2fae29a21423aad2c322
mattia33011 <mattia.iaria30@gmail.com>
2024-07-30 00:35:54 +0200

    fix: test fixed

--- src/test/kotlin/it/mag/wrongtzap/service/UserServiceTest.kt
+++ src/test/kotlin/it/mag/wrongtzap/service/UserServiceTest.kt
@@ -1,30 +1,19 @@
 package it.mag.wrongtzap.service
 
-import io.mockk.every
-import io.mockk.impl.annotations.MockK
-import io.mockk.mockk
 import it.mag.wrongtzap.model.User
 import it.mag.wrongtzap.repository.UserRepository
 import jakarta.transaction.Transactional
-import net.bytebuddy.utility.dispatcher.JavaDispatcher.Container
 import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.internal.verification.NoInteractions
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.testcontainers.context.ImportTestcontainers
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection
-import org.springframework.data.jpa.repository.Query
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.MySQLContainer
+import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
-import kotlin.test.assertIsNot
 import kotlin.test.assertNotNull
-import kotlin.test.assertNull
 
 @SpringBootTest
 @Testcontainers
@@ -41,21 +30,15 @@ class UserServiceTest @Autowired constructor(
         val default3= User("Alex" ,"trythis@arubapec.it")
 
         @Container
-        lateinit var mysql: MySQLContainer<Nothing>
-
-        @BeforeAll
-        @JvmStatic
-        fun init() {
-            mysql = MySQLContainer<Nothing>("mysql:latest").apply {
-                withInitScript("schema.sql")
-            }
-            mysql.start()
-
+        val mysql: MySQLContainer<Nothing> = MySQLContainer<Nothing>("mysql:latest").apply {
+            withInitScript("schema.sql")
         }
 
-        @DynamicPropertySource
         @JvmStatic
-        fun dynamicProperties(registry: DynamicPropertyRegistry) {
+        @BeforeAll
+        @DynamicPropertySource
+        fun dynamicProperties(@Autowired registry: DynamicPropertyRegistry) {
+            mysql.start()
             registry.add("spring.datasource.url") { mysql.jdbcUrl }
             registry.add("spring.datasource.username") { mysql.username }
             registry.add("spring.datasource.password") { mysql.password }
@@ -68,14 +51,10 @@ class UserServiceTest @Autowired constructor(
     @Test
     @Transactional
     fun `should save`(){
-
-
         service.generate(default1)
         assertNotNull(userRepository)
-
         val testUser = service.getByUsername(default1.username).firstOrNull()
         assertNotNull(testUser)
-
     }
 
 }
